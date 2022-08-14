1ebad595831eb13ef1dd563e1fa633770fb025af
PC-Ngumoha <pcngumoha@gmail.com>
2022-08-14 18:04:08 +0000

    Create 'Merge Sort' algorithm

--- sort.h
+++ sort.h
@@ -24,4 +24,5 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 #endif /* SORT_H */
