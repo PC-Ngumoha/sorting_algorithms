1ffb6ab5df2284c7f9673fa79ba6076f91988c48
PC-Ngumoha <pcngumoha@gmail.com>
2022-08-16 19:18:04 +0000

    Modified 'Quick Sort Algorithm using Hoare Partition'

--- sort.h
+++ sort.h
@@ -24,4 +24,6 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif /* SORT_H */
