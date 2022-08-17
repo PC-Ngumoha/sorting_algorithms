73451ef1e4c220dfc1ff141966f0dd17d053d7c1
PC-Ngumoha <pcngumoha@gmail.com>
2022-08-17 15:45:18 +0000

    Create 'Bubble sort' algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,11 +4,16 @@
  * bubble_sort - sorts an array in ascending order
  * @array: array of ints to sort
  * @size: size of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
     unsigned int i, j;
    	int swap;
+
+	if (size < 2)
+		return;
 
     for (i = 0; i < size - 1; i++)
     {
